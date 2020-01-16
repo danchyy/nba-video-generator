1c377af78355f660bdb4f7c3594b1343e69e33bf
Daniel Bratulic <danielbratulic@gmail.com>
2020-01-16 08:23:25 +0100

    Added more options

--- nba_video_generator/video_creator.py
+++ nba_video_generator/video_creator.py
@@ -1,6 +1,7 @@
 import time
 import subprocess
 import argparse
+import os
 
 import pandas as pd
 
@@ -15,6 +16,7 @@ import requests
 _MADE_SHOTS = "shots_made"
 _MISSED_SHOTS = "missed_shots"
 _ASSISTS = "assists"
+_SHOTS_ASSISTS = "shots_assists"
 
 
 def get_game_for_team(team_name: str, game_date: str):
@@ -63,6 +65,11 @@ def get_all_made_shots_by_player(pbp: pd.DataFrame, player_id):
     return made_shots.loc[made_shots.PLAYER1_ID == player_id]
 
 
+def get_made_shots_and_assists(pbp: pd.DataFrame, player_id):
+    all_made_shots = get_all_made_shots(pbp)
+    return all_made_shots.loc[(all_made_shots.PLAYER2_ID == player_id) | (all_made_shots.PLAYER1_ID == player_id)]
+
+
 def get_all_missed_shots_by_player(pbp: pd.DataFrame, player_id):
     made_shots = pbp.loc[pbp.EVENTMSGTYPE == 2]
     return made_shots.loc[made_shots.PLAYER1_ID == player_id]
@@ -73,7 +80,7 @@ def get_all_assists_by_player(pbp: pd.DataFrame, player_id):
     return all_made_shots.loc[all_made_shots.PLAYER2_ID == player_id]
 
 
-def dump_videos_for_data_frame(pbp: pd.DataFrame):
+def dump_videos_for_data_frame(pbp: pd.DataFrame, target_folder: str, video_prefix: str, output_name: str):
     for index in range(len(pbp)):
         item = pbp.iloc[index]
         game_id = item.GAME_ID
@@ -84,18 +91,18 @@ def dump_videos_for_data_frame(pbp: pd.DataFrame):
         video_url = f"https://videos.nba.com/nba/pbp/media/{playlist_dict['y']}/{playlist_dict['m']}/{playlist_dict['d']}/{game_id}/{event_id}/{uuid}_1280x720.mp4"
         resp = requests.get(video_url)
 
-        with open(f'videos/doncic_ast_{index + 1}.mp4', 'wb') as f:
+        with open(f'{target_folder}/{video_prefix}_{index + 1}.mp4', 'wb') as f:
             f.write(resp.content)
             f.flush()
         print(f"Finished with {index + 1}. item out of {len(pbp)}")
         time.sleep(1)
 
-    lines = [f"file videos/doncic_ast_{index + 1}.mp4\n" for index in range(len(pbp))]
+    lines = [f"file {target_folder}/{video_prefix}_{index + 1}.mp4\n" for index in range(len(pbp))]
 
-    with open('video_list.txt', 'w') as f:
+    with open(f'{target_folder}.txt', 'w') as f:
         f.writelines(lines)
 
-    subprocess.call(["ffmpeg", "-safe", "0", "-f", "concat", "-i", "video_list.txt", "-c", "copy", "output.mp4"])
+    subprocess.call(["ffmpeg", "-safe", "0", "-f", "concat", "-i", f"{target_folder}.txt", "-c", "copy", f"{output_name}.mp4"])
 
 
 if __name__ == '__main__':
@@ -114,7 +121,22 @@ if __name__ == '__main__':
         "--target_highlight",
         help="Game date in format of YYYY-MM-DD",
         type=str,
-        choices=[_MADE_SHOTS, _MISSED_SHOTS, _ASSISTS]
+        choices=[_MADE_SHOTS, _MISSED_SHOTS, _ASSISTS, _SHOTS_ASSISTS]
+    )
+    parser.add_argument(
+        '--target_folder',
+        help="Folder where videos will be dumped",
+        type=str
+    )
+    parser.add_argument(
+        '--video_prefix',
+        help="Prefix of the videos",
+        type=str
+    )
+    parser.add_argument(
+        '--output_video_name',
+        help="Name of the video",
+        type=str
     )
     args = parser.parse_args()
 
@@ -127,12 +149,20 @@ if __name__ == '__main__':
 
     pbp_df = get_pbp_for_game(get_game_for_player(player_id=player_id, game_date=game_date))
 
+    target_folder = args.target_folder
+    video_prefix = args.video_prefix
+    output_name = args.output_video_name
+    if not os.path.exists(target_folder):
+        os.mkdir(target_folder)
+
     if target_highlight == _MADE_SHOTS:
         filtered_df = get_all_made_shots_by_player(pbp=pbp_df, player_id=player_id)
     elif target_highlight == _MISSED_SHOTS:
         filtered_df = get_all_missed_shots_by_player(pbp=pbp_df, player_id=player_id)
     elif target_highlight == _ASSISTS:
         filtered_df = get_all_assists_by_player(pbp=pbp_df, player_id=player_id)
+    elif target_highlight == _SHOTS_ASSISTS:
+        filtered_df = get_made_shots_and_assists(pbp=pbp_df, player_id=player_id)
     else:
         raise ValueError("Mistake")
-    dump_videos_for_data_frame(filtered_df)
+    dump_videos_for_data_frame(filtered_df, target_folder=target_folder, video_prefix=video_prefix, output_name=output_name)
